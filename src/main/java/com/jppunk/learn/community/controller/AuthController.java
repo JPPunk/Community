2c5ad78895f18532965025cb14eb63ba9d02e002
JPPunk <ljp1010@hotmail.com>
2019-10-30 13:50:45 +1100

    session done

--- src/main/java/com/jppunk/learn/community/controller/AuthController.java
+++ src/main/java/com/jppunk/learn/community/controller/AuthController.java
@@ -1,13 +1,18 @@
 package com.jppunk.learn.community.controller;
 
+import com.jppunk.learn.community.Model.User;
 import com.jppunk.learn.community.Provider.GithubProvider;
 import com.jppunk.learn.community.dto.AccessTokenDTO;
 import com.jppunk.learn.community.dto.GithubUser;
+import com.jppunk.learn.community.mapper.UserMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.UUID;
 
 @Controller
 
@@ -15,6 +20,9 @@ public class AuthController {
 
     @Autowired
     private GithubProvider githubProvider;
+
+    @Autowired
+    private UserMapper userMapper;
 
     @Value("${github.client.id}")
     private String clientId;
@@ -27,7 +35,8 @@ public class AuthController {
 
     @GetMapping("/callback")
     public String callback(@RequestParam(name="code") String code,
-                           @RequestParam(name="state") String state){
+                           @RequestParam(name="state") String state,
+                           HttpServletRequest request){
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
         accessTokenDTO.setCode(code);
         accessTokenDTO.setClient_id(clientId);
@@ -37,8 +46,22 @@ public class AuthController {
         String accessToken = githubProvider.getAccessToken(accessTokenDTO);
         GithubUser githubUser = githubProvider.getUser(accessToken);
         System.out.println(githubUser.getId());
-
-        return "index";
+        if (githubUser != null){
+            //登录成功，写cookie和session
+            User user = new User();
+            user.setToken(UUID.randomUUID().toString());
+            user.setName(githubUser.getName());
+            user.setAccountId(String.valueOf(githubUser.getId()));
+            user.setGmtCreate(System.currentTimeMillis());
+            user.setGmtModified(user.getGmtCreate());
+            userMapper.insert(user);
+            request.getSession().setAttribute("user", githubUser);
+            return "redirect:/";
+        }
+        else{
+            //登陆失败
+            return "redirect:/";
+        }
     }
 
 }
---
+++ src/main/java/com/jppunk/learn/community/mapper/UserMapper.java
@@ -0,0 +1,11 @@
+package com.jppunk.learn.community.mapper;
+
+import com.jppunk.learn.community.Model.User;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface UserMapper {
+    @Insert("insert into user (name, account_id, token, gmt_create, gmt_modified) values (#{name}, #{accountId}, #{token}, #{gmtCreate}, #{gmtModified})")
+    void insert(User user);
+    }
