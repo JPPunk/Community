bc04b8b82ffc31bbc1780199938f0f10021cd1f3
JPPunk <ljp1010@hotmail.com>
2019-10-31 00:54:51 +1100

    flyway migrate

--- README.md
+++ README.md
@@ -3,3 +3,18 @@
 [Spring 文档](https://spring.io/guides)
 [Bootstrap组件](https://v3.bootcss.com/components/)
 [OkHttp](https://square.github.io/okhttp/)
+
+# 脚本
+```sql
+create table USER
+(
+    ID           INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
+    ACCOUNT_ID   VARCHAR(100),
+    NAME         VARCHAR(50),
+    TOKEN        CHAR(36),
+    GMT_CREATE   BIGINT,
+    GMT_MODIFIED BIGINT,
+    constraint USER_PK
+        primary key (ID)
+);
+```
--- src/main/java/com/jppunk/learn/community/mapper/UserMapper.java
+++ src/main/java/com/jppunk/learn/community/mapper/UserMapper.java
@@ -3,9 +3,13 @@ package com.jppunk.learn.community.mapper;
 import com.jppunk.learn.community.Model.User;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
 
 @Mapper
 public interface UserMapper {
     @Insert("insert into user (name, account_id, token, gmt_create, gmt_modified) values (#{name}, #{accountId}, #{token}, #{gmtCreate}, #{gmtModified})")
     void insert(User user);
-    }
+
+    @Select("select * from user where token= #{token}")
+    User findByToken(String token);
+}
